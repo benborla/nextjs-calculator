8ddeb9320e9eab51b9bf2671a41e8b5b0d46622b
Ben Borla <benborla@icloud.com>
2024-12-18 14:12:12 +0800

    chore: updated lint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,37 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js'
+import globals from 'globals'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  js.configs.recommended,
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      indent: ['error', 2],
+      'no-tabs': ['error'],
+      'comma-dangle': ['error', 'always-multiline'],
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        ...globals.node,
+      },
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
